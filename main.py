1596fee3b1afcb196eb4258ca70a80acdb214aed
simon <cloud9209@gmail.com>
2014-07-28 21:50:23 +0900

    웹툰 크롤링하는 것

--- main.py
+++ main.py
@@ -1,15 +1,30 @@
 from flask import Flask, render_template
+from urllib import urlopen
+from bs4 import BeautifulSoup
+
 app = Flask(__name__)
 app.config['DEBUG'] = True
 
 # Note: We don't need to call run() since our application is embedded within
 # the App Engine WSGI application server.
+# -*- coding: utf-8 -*-
+@app.route('/webtoon')
+def webtoon():
+	data = urlopen('http://comics.nate.com/webtoon/detail.php?btno=64923&bsno=369672&category=1').read()
+	soup = BeautifulSoup(data)
+	img_src = []
+
+	for img in soup.select('div.toonView img'):
+		img_src.append(img['src'])
+	
+	return render_template('webtoon.html', images = img_src)
 
 
 @app.route('/')
 def hello():
     """Return a friendly HTTP greeting."""
     return render_template('index.html')
+
 
 @app.route('/ideada')
 def ideada():
