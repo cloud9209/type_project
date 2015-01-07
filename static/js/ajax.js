ff1331ed481fccc97ba338954db49ef5674e9223
simon <cloud9209@gmail.com>
2015-01-07 17:17:10 +0900

    push

--- static/js/ajax.js
+++ static/js/ajax.js
@@ -1,34 +1,25 @@
 $(document).ready(function(){
-	$('#search_button').click(function(){
-		var query = $('#search_text').val();
-		$('#search_text').val(""); 
-		$('#twit_div').empty();
-		
-		$.ajax({
-			url : '/tweepy_query',
-			type : 'post',
-			data : {"query" : query},
+	var n = 0;
 
-			success : function(datas){
-				var json_datas = $.parseJSON(datas);
-				var content = "<table id = 'twit_table'><tr><td class='twit_name'>Name</td><td class='twit_name td_right'>Content</td></tr>";
+	$('.into_the_proj').click(function(){
+		if(n == 0){	
+			$.ajax({
+				type : 'POST',
+				url : '/inside_proj',
+				data : '',
 
-				for(i=0; i < json_datas.length; i++){
-					content += "<tr class='twit_table_tr'><td class='twit_contents td_left'>" + json_datas[i].user_name + "</td><td class='twit_contents td_right td_right_contents'>" + json_datas[i].text + "</td></tr>";
+				success : function(datas){
+					$('#proj_papers').append(datas);
+				},
+
+				error : function(){
+					console.log("error");
+				},
+				complete : function(){
+					console.log("complete")
 				}
-				content += "</table>";
-
-				$('#twit_div').append(content);
-			},
-
-			error : function(){
-				console.log("error");
-			},
-			complete : function(){
-				console.log("complete")
-			}
-		});
-
+			});
+			n = 1;
+		};
 	});
-	
 });
--- static/js/idea.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function(){
-
-	$(".font_name").click(function(){
-		$(this).toggleClass("font_name_clicked");
-	});
-});
-
