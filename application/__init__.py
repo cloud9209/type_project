9c48b1971ccf41f1b448d13875e103ceb513a6f6
pjhjohn <pjhjohn@gmail.com>
2015-02-03 10:51:13 +0900

    [#27] Jinja truncate filter doesn't handle unicode string length to fit that of ascii-range string

--- application/__init__.py
+++ application/__init__.py
@@ -18,10 +18,43 @@ manager.add_command('db', MigrateCommand)
 # [Flask Extension] DebugToolbar
 from flask.ext.debugtoolbar import DebugToolbarExtension
 from application.models.image_storage import load_base64
+import logging
 app.config['DEBUG_TB_PROFILER_ENABLED'] = True
 toolbar = DebugToolbarExtension(app)
 app.jinja_env.globals.update(load_base64=load_base64)
+def unicode_truncate(_string_, length, encoding='utf-8'):
+    logging.info('TRUNCATE')
+    logging.warning(_string_)
+    logging.warning(_string_.split())
+    logging.info('--------')
+    _str_encoded_ = _string_.encode(encoding)[:length - 4]
+    return _str_encoded_.decode(encoding, 'ignore') + ' ...'
 
+# http://stackoverflow.com/questions/13665001/python-truncating-international-string
+# LENGTH_BY_PREFIX = [(0xC0, 2), (0xE0, 3), (0xF0, 4), (0xF8, 5), (0xFC, 6)] # first byte mask, total codepoint length
+
+# def codepoint_length(first_byte):
+#     if first_byte < 128:
+#         return 1 # ASCII
+#     for mask, length in LENGTH_BY_PREFIX:
+#         if first_byte & mask == mask:
+#             return length
+#     assert False, 'Invalid byte %r' % first_byte
+
+# def cut_to_bytes_length(unicode_text, byte_limit):
+#     utf8_bytes = unicode_text.encode('UTF-8')
+#     cut_index = 0
+#     logging.info(['%02x' ])
+#     while cut_index < len(utf8_bytes):
+#         step = codepoint_length(ord(utf8_bytes[cut_index]))
+#         if cut_index + step > byte_limit:
+#             # can't go a whole codepoint further, time to cut
+#             return utf8_bytes[:cut_index]
+#         else:
+#             cut_index += step
+#     # length limit is longer than our bytes strung, so no cutting
+#     return utf8_bytes
+app.jinja_env.globals.update(truncate_u = unicode_truncate)#cut_to_bytes_length)
 # Import Every function in 'controllers' directory
 from application.models import schema
 import os
