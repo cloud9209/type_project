d8cb6f1c8141f625e4e44cedcd15c413c2ddc110
pjhjohn <pjhjohn@gmail.com>
2015-02-03 15:50:46 +0900

    Slight Improvement on Truncation of UnicodeStr

--- application/__init__.py
+++ application/__init__.py
@@ -19,14 +19,16 @@ manager.add_command('db', MigrateCommand)
 # [Flask Extension] DebugToolbar
 from flask.ext.debugtoolbar import DebugToolbarExtension
 from application.models.image_storage import load_base64
-import logging
 app.config['DEBUG_TB_PROFILER_ENABLED'] = True
 toolbar = DebugToolbarExtension(app)
 app.jinja_env.globals.update(load_base64=load_base64)
 
-# http://stackoverflow.com/questions/13665001/python-truncating-international-string
-LENGTH_BY_PREFIX = [(0xFC, 6), (0xF8, 5), (0xF0, 4), (0xE0, 3), (0xC0, 2)] # first byte mask, total codepoint length
-
+# [Jinja] Unicode String Truncator
+'''
+[Reference] Cut By WordBytes : http://stackoverflow.com/questions/13665001/python-truncating-international-string
+[Reference] Cut By WordWidth : http://en.wikipedia.org/wiki/Halfwidth_and_fullwidth_forms
+'''
+LENGTH_BY_PREFIX = [(0xFC, 6), (0xF8, 5), (0xF0, 4), (0xE0, 3), (0xC0, 2)]
 def codepoint_length(first_byte):
     if first_byte < 128:
         return 1 # ASCII
@@ -35,11 +37,10 @@ def codepoint_length(first_byte):
             return length
     assert False, 'Invalid byte %r' % first_byte
 
-def cut_to_bytes_length(unicode_text, byte_limit, encoding = 'utf-8', ending = ' ...'):
-    utf8_bytes = unicode_text.encode(encoding)
+def truncate_bytelen(unicode_string, byte_limit, encoding = 'utf-8', ending = ' ...'):
+    utf8_bytes = unicode_string.encode(encoding)
     cut_index = 0
     while cut_index < len(utf8_bytes):
-        logging.info('%d : %02x' % (cut_index, ord(utf8_bytes[cut_index])))
         step = codepoint_length(ord(utf8_bytes[cut_index]))
         if cut_index + step > byte_limit:
             # can't go a whole codepoint further, time to cut
@@ -47,8 +48,9 @@ def cut_to_bytes_length(unicode_text, byte_limit, encoding = 'utf-8', ending = '
         else:
             cut_index += step
     # length limit is longer than our bytes strung, so no cutting
-    return utf8_bytes.decode(encoding, 'ignore') + ending
-app.jinja_env.globals.update(truncate_u = cut_to_bytes_length)
+    return utf8_bytes.decode(encoding, 'ignore')
+
+app.jinja_env.globals.update(truncate_u = truncate_bytelen)
 # Import Every function in 'controllers' directory
 from application.models import schema
 import os
