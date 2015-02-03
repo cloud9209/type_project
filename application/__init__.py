a0a7655f2809a377a6ab533897f840a218609a80
pjhjohn <pjhjohn@gmail.com>
2015-02-03 13:04:10 +0900

    [#27] Improvement on String truncator using Codepoint Length

--- application/__init__.py
+++ application/__init__.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 # Flask Instance
 from flask import Flask
 app = Flask('application')
@@ -22,39 +23,32 @@ import logging
 app.config['DEBUG_TB_PROFILER_ENABLED'] = True
 toolbar = DebugToolbarExtension(app)
 app.jinja_env.globals.update(load_base64=load_base64)
-def unicode_truncate(_string_, length, encoding='utf-8'):
-    logging.info('TRUNCATE')
-    logging.warning(_string_)
-    logging.warning(_string_.split())
-    logging.info('--------')
-    _str_encoded_ = _string_.encode(encoding)[:length - 4]
-    return _str_encoded_.decode(encoding, 'ignore') + ' ...'
 
 # http://stackoverflow.com/questions/13665001/python-truncating-international-string
-# LENGTH_BY_PREFIX = [(0xC0, 2), (0xE0, 3), (0xF0, 4), (0xF8, 5), (0xFC, 6)] # first byte mask, total codepoint length
+LENGTH_BY_PREFIX = [(0xFC, 6), (0xF8, 5), (0xF0, 4), (0xE0, 3), (0xC0, 2)] # first byte mask, total codepoint length
 
-# def codepoint_length(first_byte):
-#     if first_byte < 128:
-#         return 1 # ASCII
-#     for mask, length in LENGTH_BY_PREFIX:
-#         if first_byte & mask == mask:
-#             return length
-#     assert False, 'Invalid byte %r' % first_byte
+def codepoint_length(first_byte):
+    if first_byte < 128:
+        return 1 # ASCII
+    for mask, length in LENGTH_BY_PREFIX:
+        if first_byte & mask == mask:
+            return length
+    assert False, 'Invalid byte %r' % first_byte
 
-# def cut_to_bytes_length(unicode_text, byte_limit):
-#     utf8_bytes = unicode_text.encode('UTF-8')
-#     cut_index = 0
-#     logging.info(['%02x' ])
-#     while cut_index < len(utf8_bytes):
-#         step = codepoint_length(ord(utf8_bytes[cut_index]))
-#         if cut_index + step > byte_limit:
-#             # can't go a whole codepoint further, time to cut
-#             return utf8_bytes[:cut_index]
-#         else:
-#             cut_index += step
-#     # length limit is longer than our bytes strung, so no cutting
-#     return utf8_bytes
-app.jinja_env.globals.update(truncate_u = unicode_truncate)#cut_to_bytes_length)
+def cut_to_bytes_length(unicode_text, byte_limit, encoding = 'utf-8', ending = ' ...'):
+    utf8_bytes = unicode_text.encode(encoding)
+    cut_index = 0
+    while cut_index < len(utf8_bytes):
+        logging.info('%d : %02x' % (cut_index, ord(utf8_bytes[cut_index])))
+        step = codepoint_length(ord(utf8_bytes[cut_index]))
+        if cut_index + step > byte_limit:
+            # can't go a whole codepoint further, time to cut
+            return utf8_bytes[:cut_index].decode(encoding, 'ignore') + ending
+        else:
+            cut_index += step
+    # length limit is longer than our bytes strung, so no cutting
+    return utf8_bytes.decode(encoding, 'ignore') + ending
+app.jinja_env.globals.update(truncate_u = cut_to_bytes_length)
 # Import Every function in 'controllers' directory
 from application.models import schema
 import os
--- application/config.py
+++ application/config.py
@@ -3,7 +3,7 @@ from flask import url_for, request, session, redirect
 
 # Set Migration Directory for DB & DB info
 app.config.update(dict(
-    STRING_TRUNCATE_LENGTH = 50,
+    STRING_TRUNCATE_LENGTH = 150,
     DEBUG=True,
     SECRET_KEY='development key',
     SQLALCHEMY_DATABASE_URI ='mysql+gaerdbms:///type_db?instance=cloud-9209:typedesign-instance',
