008ccf10f4d3917145c4b15f1be5f819421d32c3
pjhjohn <pjhjohn@gmail.com>
2015-02-06 20:17:15 +0900

    [#31] Use files.ExistenceError for catching GCS exception

--- application/models/image_storage.py
+++ application/models/image_storage.py
@@ -2,7 +2,7 @@ from application import db
 from google.appengine.api import files, images
 import author, project, work, base64
 
-ROOT = '/gs/type-storage'
+GCS_BUCKET = '/gs/type-storage'
 
 # return success flag
 def store(category, id, raw_image) :
@@ -15,8 +15,8 @@ def store(category, id, raw_image) :
     ''' image '''
     image = raw_image.read()
     image_name = '%s/%s/%d.%s' % (category, 'image', id, ext)
-    if _target_.image != image_name : files.delete('%s/%s' % (ROOT, _target_.image))
-    path_writable = files.gs.create('%s/%s' % (ROOT, image_name), mime_type = raw_image.content_type, acl='public-read')
+    if _target_.image != image_name : files.delete('%s/%s' % (GCS_BUCKET, _target_.image))
+    path_writable = files.gs.create('%s/%s' % (GCS_BUCKET, image_name), mime_type = raw_image.content_type, acl='public-read')
     with files.open(path_writable, 'ab') as _file_ :
         _file_.write(image)
     files.finalize(path_writable)
@@ -25,8 +25,8 @@ def store(category, id, raw_image) :
     ''' thumbnail '''
     thumbnail = images.resize(image, 200, 200)
     thumbnail_name = '%s/%s/%d.%s' % (category, 'thumbnail', id, ext)
-    if _target_.thumbnail != thumbnail_name : files.delete('%s/%s' % (ROOT, _target_.thumbnail))
-    path_writable = files.gs.create('%s/%s' % (ROOT, thumbnail_name), mime_type = raw_image.content_type, acl='public-read')
+    if _target_.thumbnail != thumbnail_name : files.delete('%s/%s' % (GCS_BUCKET, _target_.thumbnail))
+    path_writable = files.gs.create('%s/%s' % (GCS_BUCKET, thumbnail_name), mime_type = raw_image.content_type, acl='public-read')
     with files.open(path_writable, 'ab') as _file_ :
         _file_.write(thumbnail)
     files.finalize(path_writable)
@@ -37,20 +37,27 @@ def store(category, id, raw_image) :
     return True
 
 def load(filename) :
-    path = '%s/%s' % (ROOT, filename)
+    path = '%s/%s' % (GCS_BUCKET, filename)
     try :
         with files.open(path, 'r') as _file_ :
             data = _file_.read()
         return data
-    except : #ExistenceError
+    except files.ExistenceError :
         return ''
+    except :
+        logging.critical('Unexpected Error on image_storage.load()')
+        raise
 
 def load_base64(filename) :
-    path, data = '%s/%s' % (ROOT, filename), ''    
+    path, data = '%s/%s' % (GCS_BUCKET, filename), ''    
     try :
         with files.open(path, 'r') as _file_ :
             data = _file_.read()
-    except : pass
+    except files.ExistenceError :
+        pass
+    except :
+        logging.critical('Unexpected Error on image_storage.load_base64()')
+        raise
     mime_type = 'image/%s' % (['png', 'jpeg'][filename.split('.')[-1] in ['jpeg', 'jpg']])
     return 'data:%s;base64,%s' % (mime_type, base64.b64encode(data))
 
@@ -61,8 +68,8 @@ def remove(category, id) :
     _target_ = eval('%s.get("id", %d, 1)' % (category, id))
     if _target_ is None : return False
 
-    files.delete('%s/%s' % (ROOT, _target_.image))
-    files.delete('%s/%s' % (ROOT, _target_.thumbnail))
+    files.delete('%s/%s' % (GCS_BUCKET, _target_.image))
+    files.delete('%s/%s' % (GCS_BUCKET, _target_.thumbnail))
     _target_.image = ''
     _target_.thumbnail = ''
     db.session.commit()
